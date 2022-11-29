273c761dffc7e01c223a9cd0493568b4248ec93b
Carlos <pinillacarlos892@gmail.com>
2022-11-29 00:14:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -40,3 +40,19 @@ if __name__ == '__main__':
 
     zeroFinder.fn = lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_))
     print(zeroFinder.get_zero())
+
+    zeroFinder = ZeroFinderByBisection(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
+    print(zeroFinder.get_zero())
+
+    zeroFinder = ZeroFinderBySecant(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
+    print(zeroFinder.get_zero())
+
+    zeroFinder = ZeroFinderByBisection(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
+    zeroFinder.initial_value = 5
+    zeroFinder.initial_value_2 = 6
+    print(zeroFinder.get_zero())
+
+    zeroFinder = ZeroFinderBySecant(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
+    zeroFinder.initial_value = 5
+    zeroFinder.initial_value_2 = 6
+    print(zeroFinder.get_zero())
