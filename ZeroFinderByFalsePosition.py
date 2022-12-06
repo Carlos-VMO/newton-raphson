e3f3b658a676d8b531ca0a87f9e2d6c9ef81875c
Carlos <carlos.pinilla@vmocentral.com>
2022-12-05 22:13:46 -0500

    Zero finder by false position

---
+++ ZeroFinderByFalsePosition.py
@@ -0,0 +1,29 @@
+import Utils as appUtils
+
+from ZeroFinderByInterval import ZeroFinderByInterval
+
+
+class ZeroFinderByFalsePosition(ZeroFinderByInterval):
+    max_i = 50
+
+    def __init__(self, fn_):
+        self.fn = fn_
+        super().__init__(fn_)
+
+    def before_init_iteration(self):
+        super().before_init_iteration()
+        self.result = appUtils.approach_to_zero_by_secant(self.fn, self._a_1, self._a_2)
+
+    def solver_function(self):
+        if self.fn(self.result) * self.fn(self._a_1) > 0:
+            self._a_1 = self.result
+        else:
+            self._a_2 = self.result
+        self.result = appUtils.approach_to_zero_by_secant(self.fn, self._a_1, self._a_2)
+
+    def get_zero(self):
+        if self.fn(self.initial_value) * self.fn(self.initial_value_2) < 0:
+            return super().get_zero()
+        else:
+            print('No ha sido posible encontrar un cero. Por favor verifique la función y el intervalo de busqueda')
+            return
--- main.py
+++ main.py
@@ -1,58 +1,35 @@
 from sympy import *
 
 from ZeroFinderByBisection import ZeroFinderByBisection
+from ZeroFinderByFalsePosition import ZeroFinderByFalsePosition
 from ZeroFinderByNewtonRaphson import ZeroFinderByNewtonRaphson
 from ZeroFinderBySecant import ZeroFinderBySecant
 
 if __name__ == '__main__':
-    zeroFinder = ZeroFinderByNewtonRaphson(lambda x_: x_ ** 5 - 6)
-    print(zeroFinder.get_zero())
+    zeroFinderByNewtonRaphson = ZeroFinderByNewtonRaphson(lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_)))
+    zeroFinderByNewtonRaphson.max_i = 10
+    print('Por newton raphson', zeroFinderByNewtonRaphson.get_zero())
 
-    zeroFinder.fn = lambda x_: x_ ** 3 - 2
-    print(zeroFinder.get_zero())
+    zeroFinderByNewtonRaphson.fn = lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9
+    print('Por newton raphson', zeroFinderByNewtonRaphson.get_zero())
 
-    zeroFinder.fn = cos
-    print(zeroFinder.get_zero())
+    zeroFinderByBisection = ZeroFinderByBisection(lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_)))
+    zeroFinderByBisection.max_i = 10
+    print('Por bisección', zeroFinderByBisection.get_zero())
 
-    zeroFinder.fn = lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_))
-    print(zeroFinder.get_zero())
+    zeroFinderByBisection.fn = lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9
+    print('Por bisección', zeroFinderByBisection.get_zero())
 
-    zeroFinder = ZeroFinderByBisection(lambda x_: x_ ** 5 - 6)
-    print(zeroFinder.get_zero())
+    zeroFinderBySecant = ZeroFinderBySecant(lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_)))
+    zeroFinderBySecant.max_i = 10
+    print('Por secante', zeroFinderBySecant.get_zero())
 
-    zeroFinder.fn = lambda x_: x_ ** 3 - 2
-    print(zeroFinder.get_zero())
+    zeroFinderBySecant.fn = lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9
+    print('Por secante', zeroFinderBySecant.get_zero())
 
-    zeroFinder.fn = cos
-    print(zeroFinder.get_zero())
+    zeroFinderByFalsePosition = ZeroFinderByFalsePosition(lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_)))
+    zeroFinderByFalsePosition.max_i = 10
+    print('Por falsa posición', zeroFinderByFalsePosition.get_zero())
 
-    zeroFinder.fn = lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_))
-    print(zeroFinder.get_zero())
-
-    zeroFinder = ZeroFinderBySecant(lambda x_: x_ ** 5 - 6)
-    print(zeroFinder.get_zero())
-
-    zeroFinder.fn = lambda x_: x_ ** 3 - 2
-    print(zeroFinder.get_zero())
-
-    zeroFinder.fn = cos
-    print(zeroFinder.get_zero())
-
-    zeroFinder.fn = lambda x_: (x_ ** 2) - (4.9 * x_) + (5.7 * cos(0.01 * x_))
-    print(zeroFinder.get_zero())
-
-    zeroFinder = ZeroFinderByBisection(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
-    print(zeroFinder.get_zero())
-
-    zeroFinder = ZeroFinderBySecant(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
-    print(zeroFinder.get_zero())
-
-    zeroFinder = ZeroFinderByBisection(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
-    zeroFinder.initial_value = 5
-    zeroFinder.initial_value_2 = 6
-    print(zeroFinder.get_zero())
-
-    zeroFinder = ZeroFinderBySecant(lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9)
-    zeroFinder.initial_value = 5
-    zeroFinder.initial_value_2 = 6
-    print(zeroFinder.get_zero())
+    zeroFinderByFalsePosition.fn = lambda x_: (x_ ** 2) - (6.8 * (x_ ** 1.01)) + 9
+    print('Por falsa posición', zeroFinderByFalsePosition.get_zero())
